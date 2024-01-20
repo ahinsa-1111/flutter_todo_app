69c71a8aa31c9c0e68194284aa51fc68d3882ce4
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-01-20 09:05:04 +0530

    commit_06

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,13 +8,39 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Column(children: [
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
           ),
-        ]),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Serch',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
       ),
     );
   }
@@ -33,7 +59,7 @@ class Home extends StatelessWidget {
           height: 40,
           width: 40,
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(60),
+            borderRadius: BorderRadius.circular(30),
             child: Image.asset('assets/flower.png'),
           ),
         ),
